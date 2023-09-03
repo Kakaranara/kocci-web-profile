af4b30f9599384bdcf8311bc3ca736a78704a9be
kakaranara <wahyukoco535@gmail.com>
2023-09-03 18:16:37 +0700

    hotfix : pollishing layout

--- src/app/component/project/projectCard.tsx
+++ src/app/component/project/projectCard.tsx
@@ -6,12 +6,12 @@ import { randomBytes, randomUUID } from "crypto"
 export default function ProjectCard() {
     return (
         <div className="flex flex-col md:flex-row md:justify-center items-center gap-6 mt-12">
-            <div className="flex flex-col flex-1 w-4/6 md:mx-24">
+            <div className="flex flex-col flex-1 w-4/6">
                 <h1 className={textStyle.title}>Healtikuy</h1>
                 <h4 className={textStyle.description}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error voluptatum molestiae aliquam repellendus optio labore voluptatibus iusto illum animi incidunt, laudantium mollitia eum expedita sint totam, sapiente, soluta quam dolorum!</h4>
             </div>
             <div className="w-2/6">
-                <Image className="md:w-1/2"
+                <Image className="md:w-1/2 ml-auto"
                     key={randomUUID()}
                     src={linkedinImg}
                     width={0}
