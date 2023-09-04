131d535da1efcd2a4eede8e554fa34f4d440e91c
kakaranara <wahyukoco535@gmail.com>
2023-09-05 03:00:09 +0700

    feat : adding templates for future data

--- src/app/component/project/project.tsx
+++ src/app/component/project/project.tsx
@@ -4,18 +4,22 @@ import linkedinImg from '../../../assets/images/icon/linkedin.png'
 import textStyle from "./../../../styles/text-util.module.css"
 import ProjectCard from "./projectCard"
 import { randomUUID } from "crypto"
-
+import strings from "../../../assets/strings/intro.json"
 
 export default function MyProject() {
+    const projects = strings.project;
     return (
         <div className="mt-4">
             <div id="project">
                 <h1 className={textStyles.headline}>Projects</h1>
                 <div className="flex flex-col">
-                    <ProjectCard/>
-                    <ProjectCard/>
-                    <ProjectCard/>
-
+                    {projects.map((item) => (
+                        <ProjectCard
+                            overviewDesc={item.overview}
+                            title={item.title}
+                            portofolioGif={linkedinImg} //still debug
+                        />
+                    ))}
                 </div>
             </div>
         </div>
--- src/app/component/project/projectCard.tsx
+++ src/app/component/project/projectCard.tsx
@@ -1,19 +1,24 @@
-import Image from "next/image"
+import Image, { StaticImageData } from "next/image"
 import linkedinImg from '../../../assets/images/icon/linkedin.png'
 import textStyle from "./../../../styles/text-util.module.css"
 import { randomBytes, randomUUID } from "crypto"
 
-export default function ProjectCard() {
+export default function ProjectCard(props: {
+    title: string,
+    overviewDesc: string,
+    portofolioGif : any 
+}) {
+    const {title, overviewDesc, portofolioGif: image} = props
     return (
         <div className="flex flex-col md:flex-row md:justify-center items-center gap-6 mt-12">
             <div className="flex flex-col flex-1 w-4/6">
-                <h1 className={textStyle.title}>Healtikuy</h1>
-                <h4 className={textStyle.description}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error voluptatum molestiae aliquam repellendus optio labore voluptatibus iusto illum animi incidunt, laudantium mollitia eum expedita sint totam, sapiente, soluta quam dolorum!</h4>
+                <h1 className={textStyle.title}>{title}</h1>
+                <h4 className={textStyle.description}>{overviewDesc}</h4>
             </div>
             <div className="w-2/6">
                 <Image className="md:w-1/2 ml-auto"
                     key={randomUUID()}
-                    src={linkedinImg}
+                    src={image}
                     width={0}
                     height={0}
                     alt="s" />
