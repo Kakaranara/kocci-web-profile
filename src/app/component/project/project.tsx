7715b0f696b6ff8ef7acbc4621ce2b24e23882bb
kakaranara <wahyukoco535@gmail.com>
2023-09-07 01:52:12 +0700

    feat : adding next project feature

--- src/app/component/project/project.tsx
+++ src/app/component/project/project.tsx
@@ -7,36 +7,46 @@ import bisaDesign from "../../../../public/portofolio-gif/Bisa Design.gif"
 import petCare from "../../../../public/portofolio-gif/Pet Care.gif"
 
 export default function MyProject() {
-    const projects = strings.project;
-    const getPortofolioGif = (title : string) => {
-        switch (title) {
-          case "Healtikuy":
-            return healtikuy;
-          case "Disaster Tracker":
-            return disasterTracker;
-          case "Bisa Design Academy":
-            return bisaDesign;
-          case "Pet Care":
-            return petCare;
-          default:
-            // Handle any other cases or provide a default image
-            throw TypeError()
-        }
-      };
-    return (
-        <div className="mt-4">
-            <div id="project">
-                <div className="flex flex-col">
-                    {projects.map((item) => (
-                        <ProjectCard
-                            overviewDesc={item.overview}
-                            title={item.title}
-                            portofolioGif={getPortofolioGif(item.title)} 
-                            portofolioLink={item.repositoryLink}
-                        />
-                    ))}
-                </div>
-            </div>
+  const projects = strings.project;
+  const getPortofolioGif = (title: string) => {
+    switch (title) {
+      case "Healtikuy":
+        return healtikuy;
+      case "Disaster Tracker":
+        return disasterTracker;
+      case "Bisa Design Academy":
+        return bisaDesign;
+      case "Pet Care":
+        return petCare;
+      default:
+        // Handle any other cases or provide a default image
+        throw TypeError()
+    }
+  };
+
+  const getNextPorjectId = (index: number) => {
+    try {
+      return `#${projects[index + 1].title}`
+    } catch (e) {
+      return null;
+    }
+  }
+
+  return (
+    <div className="mt-4">
+      <div id="project">
+        <div className="flex flex-col">
+          {projects.map((item, index) => (
+            <ProjectCard
+              overviewDesc={item.overview}
+              title={item.title}
+              portofolioGif={getPortofolioGif(item.title)}
+              portofolioLink={item.repositoryLink}
+              nextProjectId={getNextPorjectId(index)}
+            />
+          ))}
         </div>
-    )
+      </div>
+    </div>
+  )
 }
--- src/app/component/project/projectCard.tsx
+++ src/app/component/project/projectCard.tsx
@@ -2,14 +2,16 @@ import Image, { StaticImageData } from "next/image"
 import textStyle from "./../../../styles/text-util.module.css"
 import { randomUUID } from "crypto"
 import Link from "next/link"
+import { AiOutlineArrowRight } from "react-icons/ai"
 
 export default function ProjectCard(props: {
     title: string,
     overviewDesc: string,
     portofolioGif: StaticImageData,
-    portofolioLink: string
+    portofolioLink: string,
+    nextProjectId: string | null,
 }) {
-    const { title, overviewDesc, portofolioGif, portofolioLink } = props
+    const { title, overviewDesc, portofolioGif, portofolioLink, nextProjectId } = props
 
     const renderLink = () => {
         if (portofolioLink.startsWith("http")) {
@@ -23,12 +25,26 @@ export default function ProjectCard(props: {
         }
     }
 
+    const renderNextProject = () => {
+        if (nextProjectId != null) {
+            return (
+                <div className="w-fit mt-8 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+                    <a href={nextProjectId} className="flex flex-row items-center scroll-smooth">
+                        <h1 className="mr-2">Go to next project</h1>
+                        <AiOutlineArrowRight />
+                    </a>
+                </div>
+            )
+        }
+    }
+
     return (
-        <div className="flex flex-col md:flex-row md:justify-center items-center gap-6 mt-20 cursor-pointer active:bg-gray-300 active:rounded-2xl transition duration-500">
+        <div id={title} className="flex flex-col md:flex-row md:justify-center items-center gap-6 mt-20 cursor-pointer active:bg-gray-300 active:rounded-2xl transition duration-500">
             <div className="flex flex-col flex-1 w-full md:w-4/6">
                 <h1 className={textStyle.title}>{title}</h1>
-                <h4 className={textStyle.description}>{overviewDesc}</h4>
-                {renderLink()} 
+                <h4 className={`${textStyle.description} mt-4`}>{overviewDesc}</h4>
+                {renderLink()}
+                {renderNextProject()}
             </div>
             <div className=" w-full md:w-2/6 flex flex-col items-start md:items-end">
                 <div className="border-[4px] border-gray-100 w-[70%] rounded-2xl overflow-hidden shadow-[15px_15px_30px_-15px_rgba(0,0,0,0.3)]">
