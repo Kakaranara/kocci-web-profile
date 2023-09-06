4fa4052b25ec5063d54ad029f55853ff43512cd5
kakaranara <wahyukoco535@gmail.com>
2023-09-06 12:10:03 +0700

    refactor : adjusting card

--- src/app/component/project/projectCard.tsx
+++ src/app/component/project/projectCard.tsx
@@ -5,22 +5,24 @@ import { randomUUID } from "crypto"
 export default function ProjectCard(props: {
     title: string,
     overviewDesc: string,
-    portofolioGif : StaticImageData 
+    portofolioGif: StaticImageData
 }) {
-    const {title, overviewDesc, portofolioGif: image} = props
+    const { title, overviewDesc, portofolioGif: image } = props
     return (
-        <div className="flex flex-col md:flex-row md:justify-center items-center gap-6 mt-12 cursor-pointer">
+        <div className="flex flex-col md:flex-row md:justify-center items-center gap-6 mt-20 cursor-pointer">
             <div className="flex flex-col flex-1 w-4/6">
                 <h1 className={textStyle.title}>{title}</h1>
                 <h4 className={textStyle.description}>{overviewDesc}</h4>
             </div>
-            <div className="w-2/6">
-                <Image className="md:w-1/2 ml-auto"
-                    key={randomUUID()}
-                    src={image}
-                    width={0}
-                    height={0}
-                    alt="s" />
+            <div className="w-2/6 flex flex-col items-end">
+                <div className="border-[5px] border-gray-50 rounded-2xl overflow-hidden">
+                    <Image className="rounded-sm"
+                        key={randomUUID()}
+                        src={image}
+                        width={0}
+                        height={500}
+                        alt="s" />
+                </div>
             </div>
         </div>
     )
