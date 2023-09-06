23887ca02a4fdca18288f5956fb49dbb6080d80b
kakaranara <wahyukoco535@gmail.com>
2023-09-06 15:44:41 +0700

    removing projet title section

--- src/app/component/project/project.tsx
+++ src/app/component/project/project.tsx
@@ -26,7 +26,6 @@ export default function MyProject() {
     return (
         <div className="mt-4">
             <div id="project">
-                <h1 className={textStyles.headline}>Projects</h1>
                 <div className="flex flex-col">
                     {projects.map((item) => (
                         <ProjectCard
